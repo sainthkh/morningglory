0e60e7f3ff093eea76b166950ec7c71b731fac2e
sainthkh <sainthkh@naver.com>
2015-12-29 16:12:36 +0900

    Added default urls and views.

---
+++ morningglory/blog/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+from . import views
+
+app_name = 'blog'
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^blog$', views.list_post, name='list_post'),
+    url(r'^page/(?P<page>[0-9]+)', views.list_post_paged, name='list_post_paged'),
+    url(r'^(?P<year>[0-9]+)/(?P<month>[0-9]+)/(?P<date>[0-9]+)/(?P<slug>[-_\w]+)/$', views.single_post, name='single_post'),
+    url(r'^categories/(?P<slug>[-_\w]+)/$', views.category, name='category'),
+    url(r'^categories/(?P<slug>[-_\w]+)/page/(?P<page>[0-9]+)$', views.category_paged, name='category_paged'),
+]
--- morningglory/blog/views.py
+++ morningglory/blog/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    pass
+   
+def list_post(request):
+    pass
+   
+def list_post_paged(request):
+    pass
+
+def single_post(request):
+    pass
+
+def category(request):
+    pass
+
+def category_paged(request):
+    pass
+
