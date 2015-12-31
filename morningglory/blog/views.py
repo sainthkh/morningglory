e58b91578c66d99b71c60b3161c7ea8573e0d94e
sainthkh <sainthkh@naver.com>
2015-12-31 18:45:43 +0900

    Added index page view and template.

--- morningglory/blog/views.py
+++ morningglory/blog/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    pass
+    return render(request, 'blog/index.html', {})
    
 def list_post(request):
     pass
