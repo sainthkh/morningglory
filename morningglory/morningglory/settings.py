7b7452cc6cf6d6b012ea5f6e8f6d5295b619f5d6
sainthkh <sainthkh@naver.com>
2015-12-31 18:45:21 +0900

    Added mongodb setting.

--- morningglory/morningglory/settings.py
+++ morningglory/morningglory/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog.apps.BlogConfig',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -76,8 +77,8 @@ WSGI_APPLICATION = 'morningglory.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'mongoengine_in_django',
+        'NAME': 'morningglory_db',
     }
 }
 
