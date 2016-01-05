f0c92de6130f4e7f0ecaf679f2d42e1f85e6a444
sainthkh <sainthkh@naver.com>
2016-01-06 08:44:01 +0900

    Changed from _ to - for url names for consistency.

--- morningglory/blog/urls.py
+++ morningglory/blog/urls.py
@@ -10,11 +10,11 @@ sitemaps = {
 app_name = 'blog'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^blog$', views.list_post, name='list_post'),
-    url(r'^page/(?P<page>[0-9]+)', views.list_post_paged, name='list_post_paged'),
-    url(r'^(?P<year>[0-9]+)/(?P<month>[0-9]+)/(?P<date>[0-9]+)/(?P<slug>[-_\w]+)/$', views.single_post, name='single_post'),
+    url(r'^blog$', views.list_post, name='list-post'),
+    url(r'^page/(?P<page>[0-9]+)', views.list_post_paged, name='list-post-paged'),
+    url(r'^(?P<year>[0-9]+)/(?P<month>[0-9]+)/(?P<date>[0-9]+)/(?P<slug>[-_\w]+)/$', views.single_post, name='single-post'),
     url(r'^categories/(?P<slug>[%-_\w]+)/$', views.category, name='category'),
-    url(r'^categories/(?P<slug>[%-_\w]+)/page/(?P<page>[0-9]+)$', views.category_paged, name='category_paged'),
+    url(r'^categories/(?P<slug>[%-_\w]+)/page/(?P<page>[0-9]+)$', views.category_paged, name='category-paged'),
     
     #sitemap
     url(r'^sitemap.xml$', sitemap, { "sitemaps": sitemaps }, name='django.contrib.sitemaps.views.sitemap'),
