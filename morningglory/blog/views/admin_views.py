07442a482b0552e0f8632f61582e97950c05b362
sainthkh <sainthkh@naver.com>
2016-01-20 11:00:56 +0900

    Moved functions which are not related with views to views/utils.py.

--- morningglory/blog/views/admin_views.py
+++ morningglory/blog/views/admin_views.py
@@ -1,12 +1,10 @@
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from blog.models import *
-from datetime import datetime
-from blog.utils import slugify, template_to_html
-from urllib.parse import quote, unquote
+from blog.utils import template_to_html
+from urllib.parse import unquote
 from blog.expanders import expand_content
-from blog.urls.shortcuts import get_post_url_by_slug
-from .utils import normalize_slug
+from .utils import *
 
 # Create your views here.
 def dashboard(request):
@@ -84,60 +82,6 @@ def category_list(request):
 	pass
 
 #
-# Writing Helpers
-# 
-# These are common helper functions for writing types like Post, Series, Views 
-#
-##############################################################
-
-def setup_writing_for_save(writing_type, request):
-	writing = get_writing(writing_type, request.POST["slug"])
-	is_edit = request.POST['slug'] != ""
-	setup_dates(writing, is_edit)
-	
-	if not is_edit:
-		writing.slug = create_slug(writing_type, request.POST["title"]) 
-	
-	setup_basic_content(writing, request.POST)
-	
-	return writing
-
-def get_writing(writing_type, slug):
-	if slug != "":
-		writing = writing_type.objects(slug=slug)[0]
-	else:
-		writing = writing_type()
-	return writing
-
-def setup_dates(writing, is_edit):
-	if is_edit:
-		writing.last_modified_date = datetime.now()
-	else:
-		writing.published_date = writing.last_modified_date = datetime.now()
-
-def create_slug(writing_type, title):
-	slug_base = slugify(title)
-	final_slug = slug_base
-	exist = writing_type.objects(slug=slug_base).count() != 0
-
-	if exist:
-		num = 1
-		while True:
-			slug_candidate = slug_base + '-' + str(num)
-			exist = writing_type.objects(slug=slug_candidate).count() != 0
-			if not exist:
-				break
-		final_slug = slug_candidate
-	
-	return final_slug	
-
-def setup_basic_content(writing, POST):
-	writing.title = POST['title']
-	writing.content = POST['content']
-	writing.excerpt = POST['excerpt']
-	writing.key_points = POST['key-points']
-
-#
 # Activity Views
 #
 ##############################################################
@@ -193,8 +137,8 @@ def save_settings(request):
 #################################################################
 
 def save_comment(request, slug):
-	comment = __setup_comment(request)
-	__save_comment(comment, slug)
+	comment = setup_comment(request)
+	save_comment_to_db(comment, slug)
 	
 	return redirect('blog:distribute-post', slug=unquote(slug))
 
@@ -206,58 +150,10 @@ def save_comment_ajax(request, slug):
 		response_data['msg'] = 'Name should not be empty.'
 	else:
 		response_data['success'] = True
-		comment = __setup_comment(request)
-		__save_comment(comment, slug)
+		comment = setup_comment(request)
+		save_comment_to_db(comment, slug)
 		response_data['html'] = template_to_html('blog/comment.html', {
 				"comment": comment,
 			})
 
 	return JsonResponse(response_data)
-
-def __setup_comment(request):
-	comment = Comment()
-	comment.name = request.POST['name']
-	comment.email = request.POST['email']
-	comment.website = request.POST['website'].strip()
-	if comment.website != '' and re.match('https?://.+', comment.website) == None:
-		comment.website = 'http://' + comment.website
-	comment.content = request.POST['comment']
-	comment.time = datetime.now()
-	return comment
-
-def __save_comment(comment, slug):
-	spam = is_spam(comment.name, comment.content)
-	if not spam:
-		activity = CommentActivity()
-	else:
-		activity = SpamCommentActivity()
-	
-	activity.comment = comment
-	activity.date = comment.time
-	
-	post = Post.objects(slug=slug)[0]
-	activity.slug = slug
-	activity.title = post.title
-	
-	activity.save()
-	
-	if not spam:
-		post.comments.append(comment)
-		post.save()	
-
-def is_spam(content, author):
-	from pykismet3 import Akismet
-	import os
-	
-	a = Akismet(blog_url="http://wiseinit.com",
-				user_agent="WiseInit System/0.0.1")
-
-	a.api_key=""
-	
-	return a.check({'user_ip': os.environ['REMOTE_ADDR'],
-			'user_agent': os.environ['HTTP_USER_AGENT'],
-			'referrer': os.environ.get('HTTP_REFERER', 'unknown'),
-			'comment_content': content,
-			'comment_author': author,
-		})
-	
--- morningglory/blog/views/utils.py
+++ morningglory/blog/views/utils.py
@@ -1,7 +1,121 @@
 from urllib.parse import quote, unquote
+from blog.models import *
+from blog.utils import slugify
+from datetime import datetime
+from django.conf import settings
 
 def normalize_slug(slug):
 	if "%" not in slug:
 		slug = quote(slug)
 	
 	return slug
+
+#
+# Writing Helpers
+# 
+# These are common helper functions for writing types like Post, Series, Views 
+#
+##############################################################
+
+def setup_writing_for_save(writing_type, request):
+	writing = get_writing(writing_type, request.POST["slug"])
+	is_edit = request.POST['slug'] != ""
+	setup_dates(writing, is_edit)
+	
+	if not is_edit:
+		writing.slug = create_slug(writing_type, request.POST["title"]) 
+	
+	setup_basic_content(writing, request.POST)
+	
+	return writing
+
+def get_writing(writing_type, slug):
+	if slug != "":
+		writing = writing_type.objects(slug=slug)[0]
+	else:
+		writing = writing_type()
+	return writing
+
+def setup_dates(writing, is_edit):
+	if is_edit:
+		writing.last_modified_date = datetime.now()
+	else:
+		writing.published_date = writing.last_modified_date = datetime.now()
+
+def create_slug(writing_type, title):
+	slug_base = slugify(title)
+	final_slug = slug_base
+	exist = writing_type.objects(slug=slug_base).count() != 0
+
+	if exist:
+		num = 1
+		while True:
+			slug_candidate = slug_base + '-' + str(num)
+			exist = writing_type.objects(slug=slug_candidate).count() != 0
+			if not exist:
+				break
+		final_slug = slug_candidate
+	
+	return final_slug	
+
+def setup_basic_content(writing, POST):
+	writing.title = POST['title']
+	writing.content = POST['content']
+	writing.excerpt = POST['excerpt']
+	writing.key_points = POST['key-points']
+
+#
+# Comment Savers. 
+#
+#############################################################
+
+def setup_comment(request):
+	comment = Comment()
+	comment.name = request.POST['name']
+	comment.email = request.POST['email']
+	comment.website = request.POST['website'].strip()
+	if comment.website != '' and re.match('https?://.+', comment.website) == None:
+		comment.website = 'http://' + comment.website
+	comment.content = request.POST['comment']
+	comment.time = datetime.now()
+	return comment
+
+def save_comment_to_db(comment, slug):
+	spam = is_spam(comment.name, comment.content)
+	if not spam:
+		activity = CommentActivity()
+	else:
+		activity = SpamCommentActivity()
+	
+	activity.comment = comment
+	activity.date = comment.time
+	
+	post = Post.objects(slug=slug)[0]
+	activity.slug = slug
+	activity.title = post.title
+	
+	activity.save()
+	
+	if not spam:
+		post.comments.append(comment)
+		post.save()	
+
+def is_spam(content, author):
+	if settings.DEBUG:
+		return False
+		
+	from pykismet3 import Akismet
+	import os
+	
+	a = Akismet(blog_url="http://wiseinit.com",
+				user_agent="WiseInit System/0.0.1")
+
+	a.api_key= Secret.objects(name='akismet')[0].key
+	
+	return a.check({'user_ip': os.environ['REMOTE_ADDR'],
+			'user_agent': os.environ['HTTP_USER_AGENT'],
+			'referrer': os.environ.get('HTTP_REFERER', 'unknown'),
+			'comment_content': content,
+			'comment_author': author,
+		})
+	
