a3872ab160d75a27d8531727da967663c4030ac3
sainthkh <sainthkh@naver.com>
2016-01-20 12:02:43 +0900

    Created share template_tag.

--- morningglory/blog/templatetags/blog_tags.py
+++ morningglory/blog/templatetags/blog_tags.py
@@ -1,6 +1,9 @@
 from django import template
 from django.utils.safestring import mark_safe
+from urllib.parse import quote, unquote
 import mistune
+
+from blog.urls.shortcuts import get_post_url_by_slug
 
 register = template.Library()
 
@@ -9,3 +12,15 @@ def markdown_format(text):
 	r = mistune.Renderer(escape=False)
 	markdown = mistune.Markdown(renderer=r)
 	return markdown(text)
+
+@register.inclusion_tag('blog/share.html', name="share", takes_context=True)
+def share_div(context, writing):
+	request = context['request']
+
+	share = {}
+	share['title'] = quote(writing.title)
+	share['url'] = request.build_absolute_uri(quote(get_post_url_by_slug(writing.slug)))
+	
+	return {
+		"share": share
+	}
