4ebae2b6b3dc2cfc6ebaed76e2ae95cb79f112d7
sainthkh <sainthkh@naver.com>
2016-01-23 10:34:42 +0900

    Changed restriction. Changed space to tabs.

--- morningglory/blog/models/email_models.py
+++ morningglory/blog/models/email_models.py
@@ -2,15 +2,15 @@ from django.db import models
 from mongoengine import *
 
 class Subscriber(DynamicDocument):
-    first_name = StringField(max_length=128)
-    email = StringField(max_length=254, unique=True)
+	first_name = StringField(max_length=128)
+	email = StringField(max_length=254, unique=True)
 
 class Email(DynamicDocument):
-    title = StringField(max_length=512)
-    content = StringField(max_length=30000)
-    def __str__(self):
-        return self.title
+	slug = StringField(unique=True)
+	title = StringField()
+	content = StringField()
 
 class EmailList(DynamicDocument):
-    name = StringField(max_length=128)
-    subscribers = ListField(ReferenceField('Subscriber'))
+	slug = StringField(unique=True)
+	name = StringField()
+	subscribers = ListField(ReferenceField('Subscriber'))
